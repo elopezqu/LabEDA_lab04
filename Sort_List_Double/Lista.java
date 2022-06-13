b15200671ca1b9068ef55e51865a59bbbc598217
Edson López <elopezqu@unsa.edu.pe>
2022-06-12 22:50:02 -0500

    Se crea la clase Lista y se agregan los métodos get y toString

---
+++ Sort_List_Double/Lista.java
@@ -0,0 +1,48 @@
+public class Lista<E>{
+    private Node<E> inicio,fin;
+    private int tamaño;
+    public Lista(){
+        this.inicio = this.fin = null;
+        this.tamaño = 0;
+    }
+    public boolean isEmty(){
+        return true;
+    }
+    public int getTamaño(){
+        return tamaño;
+    }
+    public void agregarAlFinal(E dato){
+        
+    }
+    public void agregarAlInicio(E dato) {
+
+    }
+
+    public Node<E> getInicio(int p) {
+        Node<E> aux = this.inicio;
+        for (int i = 0; i < p; i++) {
+            aux = aux.getNext();
+        }
+        return aux;
+    }
+
+    public Node<E> getAtras(int p) {
+        Node<E> aux = this.fin;
+        for (int i = this.tamaño - 1; i > p; i--) {
+            aux = aux.getPrevious();
+        }
+        return aux;
+    }
+
+    public String toString() {
+        String aux = "";
+        if (!isEmty()) {
+            Node<E> aux2 = this.inicio;
+            while (aux2 != null) {
+                aux += aux2.getData() + " ";
+                aux2 = aux2.getNext();
+            }
+        }
+        return aux;
+    }
+}
