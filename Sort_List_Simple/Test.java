5e116456298573b742b0dad58fe6f69362775d93
oliver092377 <omayta@unsa.edu.pe>
2022-06-12 20:37:29 -0500

    modificacion de métodos

--- Sort_List_Simple/Test.java
+++ Sort_List_Simple/Test.java
@@ -33,30 +33,28 @@ public class Main {
 		p.plot();
 
 	}
-	public static int[] generarPeorCaso(int t) {
-		int[] lista= new int[t];
+	public static Lista<Integer> generarPeorCaso(int t) {
+		Lista<Integer> misLista = new Lista<Integer>();
 		for (int i=0; i<t; i++) {
-			lista[i] = t-i;		
+			misLista.insert(t-i);	
 		}
-		return lista;		
+		return misLista;		
 	}
-	public static long insertionSort(int[] A) {
+	public static long insertionSort(Lista<Integer> A) {
 		int key;
 		int i;
 		long nano_startTime = System.nanoTime(); 
-		for(int j=1; j<A.length; j=j+1) {
-			key = A[j];
-			//Insertar A[j] en la secuencia ordenada A[1..j-1]
+		for(int j=1; j<A.size(); j=j+1) {
+			key = A.get(j).getData();
 			i = j-1;
-			while(i>-1 && A[i]>key) {
-				A[i+1] = A[i];
+			while(i>-1 && A.get(i).getData()>key) {
+				A.get(i+1).setData(A.get(i).getData());
 				i = i-1;
 			}
-			A[i+1] = key;
-			//imprimirArreglo(A);
+			A.get(i+1).setData(key); 
 		}
-		long nano_endTime = System.nanoTime();		
-		return nano_endTime - nano_startTime;		
+		long nano_endTime = System.nanoTime();
+		return nano_endTime - nano_startTime;
 	}
 
 } 
