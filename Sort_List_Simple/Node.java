cb0f5a1e59946008fea190734fa6ea21197f87cb
oliver092377 <omayta@unsa.edu.pe>
2022-06-12 20:25:33 -0500

    creación de Test.java y Lista.java

---
+++ Sort_List_Simple/Node.java
@@ -0,0 +1,29 @@
+
+public class Node<E> {
+	private E data;
+	private Node<E> next;
+	
+	public Node(E data, Node<E> next) {
+		this.data = data;
+		this.next = next;
+	}
+	public Node(E data) {
+		this(data, null);
+	}
+	public E getData() {
+		return this.data;
+	}
+	public void setData (E data) {
+		this.data = data;
+	}
+	public Node<E> getNextNode() {
+		return this.next;
+	}
+	public void setNextNode(Node<E> next) {
+		this.next = next;
+	}
+	public String toString() {
+		return this.data.toString();
+	}
+	
+}
