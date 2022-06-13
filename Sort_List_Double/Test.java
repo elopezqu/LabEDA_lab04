f47f8aea79d1597d5de6064bda869805145f2de1
Diego Pacori <dpacoria@unsa.edu.pe>
2022-06-12 23:09:04 -0500

    Se agrego el metodo insertionSort

--- Sort_List_Double/Test.java
+++ Sort_List_Double/Test.java
@@ -13,7 +13,30 @@ public class Test {
     }
 
     public static long insertionSort(Lista<Integer> A) {
-        return
+        int key;
+		int i;
+		long nano_startTime = System.nanoTime(); 
+		for(int j=1; j<A.getTamaño(); j=j+1) {
+			if(A.getTamaño()/2<j){
+				key = A.getAtras(j).getData();
+				i = j - 1;
+				while (i > -1 && A.getAtras(i).getData() > key) {
+					A.getAtras(i + 1).setData(A.getAtras(i).getData());
+					i = i - 1;
+				}
+				A.getAtras(i + 1).setData(key);
+			}else{
+				key = A.getInicio(j).getData();
+				i = j - 1;
+				while (i > -1 && A.getInicio(i).getData() > key) {
+					A.getInicio(i + 1).setData(A.getInicio(i).getData());
+					i = i - 1;
+				}
+				A.getInicio(i + 1).setData(key);
+			}
+		}
+		long nano_endTime = System.nanoTime();
+		return nano_endTime - nano_startTime;
     }
 
 }
